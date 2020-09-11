e9f9e1f4a272bb9e56fd52b44bc60f4d462c2745
Jason Ma <jason@madebyon.com>
2020-09-11 15:16:34 +0800

    subscription bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,73 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
 import Logo from "../images/i-want-to-book-court.png";
 
 import "./header.scss"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <h1>
-      <img src={Logo} alt="" />
-      {siteTitle}
-    </h1>
+class Header extends Component {
+  state = {
+    areaName: {
+      kowloon: '九龍',
+      hongkong: '香港島',
+      nt: '新界',
+      id: '離島'
+    },
+    subscribe: {
+      kowloon: true,
+      hongkong: true,
+      nt: true,
+      id: false
+    }
+  };
 
-    <button>
-      網站推送通知
-    </button>
-  </header>
-)
+  onChangeSubscribe = e => {
+    const key = e.target.value;
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    this.setState(state => ({
+      subscribe: {
+        ...state.subscribe,
+        [key]: !state.subscribe[key]
+      }
+    }));
+  };
 
-Header.defaultProps = {
-  siteTitle: ``,
+  render() {
+    const { siteTitle } = this.props;
+    const { subscribe } = this.state;
+    const { areaName } = this.state;
+
+    return (
+      <header>
+        <h1>
+          <img src={Logo} alt="" />
+          {siteTitle}
+        </h1>
+
+        <button>
+          網站推送通知
+        </button>
+
+        <div className="area-selection-container">
+          <p>
+            請選擇你想收到的通知內容
+          </p>
+          <div className="area-selection">
+            { Object.keys(subscribe).map((item) => {
+                return (
+                  <div className="form-group">
+                    <input id={item} type="checkbox" value={item} onChange={this.onChangeSubscribe} checked={subscribe[item]} />
+                    <label htmlFor={item} className="noselect">{areaName[item]}</label>
+                  </div>
+                )
+              })
+            }
+          </div>
+          <div className="subscribe-button noselect">
+            確認
+          </div>
+        </div>
+      </header>
+    )
+  }
 }
 
 export default Header
