4cd993623f8e0658fbd76a055fd65e0999f74492
Jason Ma <horsehcj@gmail.com>
2021-05-21 14:53:26 +0800

    Copy update

--- src/components/recruit-partners.js
+++ src/components/recruit-partners.js
@@ -23,7 +23,7 @@ const RecruitPartners = () => {
 
   return (
     <div className="recruit-partners-section">
-      <p>26/4 更新：誠邀3至4位場主/有興趣人士合作，建立一個約戰平台，將會係一個有趣嘅項目，有意請留低聯絡方法</p>
+      <p>21/5 更新: 放場資訊將會於2021年7月1日移到私人 Telegram 頻道，而本網站只會用作實時顯示場地狀況，有興趣加入 TG 頻道請留低聯絡方法</p>
       { !isSubmitted && (<div className="form-container">
           <input type="text" placeholder="Wtsapp電話 / Telegram / email" value={contact} onChange={e => setContact(e.target.value)} />
           <button className={cn("submit-btn", isLoading && "loading")} onClick={submitPartner}>提交</button>
